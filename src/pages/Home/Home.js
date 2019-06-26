7a7a3f99b2e300319a3f39cc661c39439879f3c5
Matthew Johnson <matthewjohnson@a-xrc7jjg5j.carfax.cfx>
2019-06-25 23:29:35 -0500

    lists recent games on home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,14 +1,21 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { GlobalContext } from "../../App";
 import "./home.scss";
 
 export const Home = () => {
   const { dispatch } = useContext(GlobalContext);
+  const [recentGames, setRecentGames] = useState([]);
 
   useEffect(() => {
     dispatch({ type: "CLEAR_TEAMS" });
-  });
+
+    fetch("https://umpclicker.com/api/games").then(response => {
+      response.json().then(json => {
+        setRecentGames(json);
+      });
+    });
+  }, [dispatch, setRecentGames]);
 
   return (
     <div className="home">
@@ -16,6 +23,14 @@ export const Home = () => {
       <Link className="primary-button" to="/teams">
         Pick Teams!
       </Link>
+      <h2>Current games:</h2>
+      <ul>
+        {recentGames.map((game, index) => (
+          <li key={index}>
+            {game.homeTeam} vs {game.awayTeam}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
